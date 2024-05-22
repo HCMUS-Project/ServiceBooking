075252e02b73798838c8fa780e19eb9d247a795c
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2024-05-22 23:05:24 +0700

    fix: fix exception, add cascade to db for auto delete

--- src/common/exceptions/exceptions.ts
+++ src/common/exceptions/exceptions.ts
@@ -2,9 +2,9 @@
  * Represents a custom exception that is thrown when a client request is malformed or invalid.
  * Extends the HttpException class from the '@nestjs/microservices' module.
  */
-import { RpcException } from '@nestjs/microservices';
+import { GrpcNotFoundException } from 'nestjs-grpc-exceptions';
 
-class GrpcItemNotFoundException extends RpcException {
+class GrpcItemNotFoundException extends GrpcNotFoundException {
     constructor(itemName: string) {
         super(`${itemName}`);
         this.message = JSON.stringify({
@@ -15,7 +15,7 @@ class GrpcItemNotFoundException extends RpcException {
     }
 }
 
-class GrpcItemExitException extends RpcException {
+class GrpcItemExitException extends GrpcNotFoundException {
     constructor(itemName: string) {
         super(`${itemName}`);
         this.message = JSON.stringify({
@@ -26,7 +26,7 @@ class GrpcItemExitException extends RpcException {
     }
 }
 
-class GrpcInvalidArgumentException extends RpcException {
+class GrpcInvalidArgumentException extends GrpcNotFoundException {
     constructor(error: string | object) {
         super(`${error}`);
         this.message = JSON.stringify({
